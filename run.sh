4d4705edffca48bda9add41788f77230c0100fb1
wyl <312715289@qq.com>
2017-11-13 09:58:28 +0800

    rm all and add *

--- run.sh
+++
@@ -1,4 +0,0 @@
-nohup python main.py > ./logs/nohup.out 2>&1 &
-# nohup python main.py --lr 0.01 --gpu 1 > ./logs/nohup1.out 2>&1 &
-# nohup python main.py --lr 0.005 --gpu 2 > ./logs/nohup2.out 2>&1 &
-# nohup python main.py --lr 0.001 --gpu 3 > ./logs/nohup3.out 2>&1 &
